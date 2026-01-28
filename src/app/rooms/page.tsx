65172b0c477a843e2ebefaf9568b512e92572099
eskil <eskil@blikeng.com>
2026-01-28 20:05:43 +0100

    Room improvements. Userpage improved

--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -3,6 +3,7 @@
 import GetRoomsAPI from "@/src/api/GetRoomsAPI";
 import React from "react";
 import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default function Rooms() {
   type Room = {
@@ -23,22 +24,65 @@ export default function Rooms() {
   if (response?.status === 200) {
     const res = response.data;
 
-    console.log(res);
+    if (res.length > 0) {
+      return (
+        <div>
+          {res.map((room: Room) => (
+            <div
+              key={room.id}
+              onClick={() => {
+                redirect(`/chat/${room.id}`);
+              }}
+            >
+              <h4>{room.name}</h4>
+              <button>Enter room</button>
+              <hr />
 
-    return res.map((room: Room) => (
-      <div
-        key={room.id}
-        onClick={() => {
-          redirect(`/chat/${room.id}`);
-        }}
-      >
-        <h4>{room.name}</h4>
-        <button>Enter room</button>
-        <hr />
+              <br />
+            </div>
+          ))}
 
-        <br />
-      </div>
-    ));
+          <Link
+            className="text-center border-2 border-black px-4 rounded-full mt-2 dark:border-white"
+            href={{
+              pathname: "/rooms/join",
+            }}
+          >
+            <p>Join a room</p>
+          </Link>
+          <Link
+            className="text-center border-2 border-black px-4 rounded-full mt-2 dark:border-white"
+            href={{
+              pathname: "/rooms/make",
+            }}
+          >
+            <p>Make a new room</p>
+          </Link>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <p>No rooms :(</p>
+          <Link
+            className="text-center border-2 border-black px-4 rounded-full mt-2 dark:border-white"
+            href={{
+              pathname: "/rooms/join",
+            }}
+          >
+            <p>Join a room</p>
+          </Link>
+          <Link
+            className="text-center border-2 border-black px-4 rounded-full mt-2 dark:border-white"
+            href={{
+              pathname: "/rooms/make",
+            }}
+          >
+            <p>Make a new room</p>
+          </Link>
+        </div>
+      );
+    }
   } else {
     return (
       <div>
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import UserAPI from "@/src/api/UserAPI";
+import "@/src/style/UserPage.css";
+import { forEach } from "eslint-config-next";
 
 export default function UserInfo() {
   const { loading, error, response } = UserAPI();
@@ -27,15 +29,49 @@ export default function UserInfo() {
     } = response.data;
 
     return (
-      <div>
-        <p>{username}</p>
-        <p>{bio}</p>
-        <p>{email}</p>
-        <p>{fullName}</p>
-        <p>{avatarUrl}</p>
-        <p>{birthday}</p>
-        <p>{createdAt}</p>
-        <p>{friends}</p>
+      <div className="text-center user-info">
+        <div>
+          <p>Username:</p>
+          <p>{username}</p>
+        </div>
+        <div>
+          <p>Bio</p>
+          <p>{bio}</p>
+        </div>
+        <div>
+          <p>Email</p>
+          <p>{email}</p>
+        </div>
+        <div>
+          <p>Full name</p>
+          <p>{fullName}</p>
+        </div>
+        <div>
+          <p>Avatar</p>
+          <p>{avatarUrl}</p>
+        </div>
+        <div>
+          <p>Birthday</p>
+          <p>{birthday}</p>
+        </div>
+        <div>
+          <p>Friends</p>
+          <p>{friends}</p>
+        </div>
+        <div>
+          <p>Rooms:</p>
+          {rooms && rooms.length > 0 ? (
+            rooms.map((room: { id: string; name: string }) => (
+              <p key={room.id}>{room.name}</p>
+            ))
+          ) : (
+            <p>No rooms</p>
+          )}
+        </div>
+        <div>
+          <p>Member since</p>
+          <p>{createdAt}</p>
+        </div>
       </div>
     );
   } else {

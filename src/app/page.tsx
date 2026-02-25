abc3477a5e8cae37669ee9628af2e5f09d4ecfe3
eskil <eskil@blikeng.com>
2026-02-25 13:43:15 +0100

    More pages fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import RoomsList from "@/src/app/RoomsList";
-import GetRoomsAPI from "@/src/api/GetRoomsAPI";
 
 export default async function Home() {
-  const auth = (await cookies()).get("AUTH")?.value;
+  const auth = localStorage.getItem("AUTH");
+
   if (!auth) {
     redirect("/login");
   }

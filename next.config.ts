decf58422a6929093bcb09fef154b270f99fe23c
eskil <eskil@blikeng.com>
2026-03-08 19:11:03 +0100

    export added

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   allowedDevOrigins: ["192.168.0.186", "localhost", "127.0.0.1"],
   images: {
     remotePatterns: [
